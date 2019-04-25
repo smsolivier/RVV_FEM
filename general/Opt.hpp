84e858359302bb35be28b9b5c1794a61b5fcac2f
Sam Olivier <smsolivier@gmail.com>
2019-04-25 09:40:38 -0700

    optimization macro definitions

---
+++ general/Opt.hpp
@@ -0,0 +1,15 @@
+#pragma once 
+
+// matrix optimizations 
+// #define RV_MATVEC 
+// #define RV_MATMULT // not implemented 
+
+// vector optimizations 
+// #define RV_VECADD 
+// #define RV_VECSUB 
+// #define RV_VECDIV 
+// #define RV_VECMUL 
+// #define RV_VECSCALE 
+// #define RV_VECOP 
+// #define RV_VECDOT 
+// #define RV_GETDOFS 
